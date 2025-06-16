651d4627bfa65666822cb4f6c4a4b94d73dec420
George Watson <gigolo@hotmail.co.uk>
2025-06-16 13:59:43 +0100

    wip

---
+++ scenes/fwt_config.h
@@ -0,0 +1,10 @@
+#define FWT_ENABLE_ARGUMENTS
+#define FWT_ENABLE_CONFIG
+#define FWT_CONFIG_PATH ".fwt-example.json"
+#define FWT_DYLIB_PATH "build"
+#define FWT_FIRST_SCENE "test"
+#define FWT_ASSETS_PATH "scenes/assets"
+
+#define FWT_SCENES \
+    X("test")       \
+    X("example")
